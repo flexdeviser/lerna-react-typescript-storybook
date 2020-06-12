91e63066f1bd05887e6eb3af53d2ad38efdf7ab7
Eric Wang <eric@future-grid.com.au>
2020-06-12 19:49:51 +1000

    chore(story): add options for story command

--- packages/playground/src/greeting/greeting-v1.story.tsx
+++ packages/playground/src/greeting/greeting-v1.story.tsx
@@ -2,6 +2,8 @@ import { storiesOf } from '@storybook/react';
 import { Graphs } from '@eric4hy/graphs';
 import React from 'react';
 
-storiesOf('Greeting', module).add('with title', () => {
+const stories = storiesOf('Greeting', module);
+// create story here
+stories.add('with title', () => {
   return <Graphs.Greeting title='Eric'></Graphs.Greeting>;
 });
